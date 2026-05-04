c5f418046f31a92c5a5150121dcad92080e272ac
DustyFrog <halsteadb3211@masseyhigh.school.nz>
2026-05-05 09:52:32 +1200

    Create c_02_instructions.py

---
+++ c_02_instructions.py
@@ -0,0 +1,23 @@
+def str_checker(question, available_choices, num_letters, error):
+    """returns the string if it meets anything in available_choices"""
+    while True:
+        choice = input(question).lower()
+        for item in available_choices:
+
+            if choice == item:
+                return item
+
+            # check first num_letters of letters to see if they tried to write the answer
+            elif choice == item[:num_letters]:
+                return item
+
+        print(error)
+
+# main routine
+
+while True:
+    yes_no = str_checker("Would you like to view the instructions? ", ["yes","no"], 1, "Please enter either yes or no.")
+    if yes_no == "yes":
+        print("Show Instructions")
+    else:
+        print("Not showing instructions")
